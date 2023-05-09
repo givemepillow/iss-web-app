bf19dcc7b44f2a4263f52d452fd27e3b80c4559d
Kirill Lapushinskiy <kirill@lapushinskiy.ru>
2023-05-10 00:34:02 +0300

    feat: new button

---
+++ src/components/buttons/NextButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <IconButton :padding="3" :size="props.size" :src="nextIcon" />
+</template>
+
+<script setup>
+import nextIcon from "@/assets/icons/next.svg";
+import IconButton from "@/components/buttons/IconButton.vue";
+
+const props = defineProps({
+  size: Number
+});
+
+
+</script>
