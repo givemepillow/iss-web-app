0d46da2a56c5a67e0d5b85f9f006c6afef062d4d
Kirill Lapushinskiy <kirill@lapushinskiy.ru>
2023-05-09 16:46:17 +0300

    refactor: styles updated

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
-import "./assets/cropper.css"
+import "@/assets/main.css";
+import "@/assets/cropper.css";
+import "@/assets/swiper.css";
 
 
 const app = createApp(App);
