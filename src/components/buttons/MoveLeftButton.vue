e476b0e4ed3071be5c34ec0e496b25c1cb8d9e51
Kirill Lapushinskiy <kirill@lapushinskiy.ru>
2023-05-12 02:56:22 +0300

    feat: new buttons

---
+++ src/components/buttons/MoveLeftButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <IconButton :padding="4" :size="props.size" :src="moveIcon" style="transform: rotate(90deg) scale(1, -1)" />
+</template>
+
+<script setup>
+import moveIcon from "@/assets/icons/move.svg";
+import IconButton from "@/components/buttons/IconButton.vue";
+
+const props = defineProps({
+  size: Number
+});
+</script>
+
+
---
+++ src/components/buttons/SaveButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <IconButton :padding="4" :size="props.size" :src="saveIcon" />
+</template>
+
+<script setup>
+import saveIcon from "@/assets/icons/save.svg";
+import IconButton from "@/components/buttons/IconButton.vue";
+
+const props = defineProps({
+  size: Number
+});
+</script>
